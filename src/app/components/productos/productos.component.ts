214b07b4320e03e744b73b74524ebf7ffa04060f
Franco Bellucci <belluccifranco@gmail.com>
2018-12-03 02:26:43 -0300

    Agrega imagenes al slideshow y setea propiedades globales

--- src/app/components/productos/productos.component.ts
+++ src/app/components/productos/productos.component.ts
@@ -20,15 +20,21 @@ export class ProductosComponent implements OnInit, OnDestroy {
 
   imageUrls: (string | IImage)[] = [
     {
-      url: 'https://homecamp.com.au/wp-content/uploads/2018/05/Dewit-Trowel-Bottle-Opener.jpg',
-      caption: 'Producto',
-      href: '/producto/2809'
+      url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1543814304/slideshow/gladiator-pro.png',
+      href: '/productos;busqueda=gladiator'
     },
     {
-      url: 'https://img.alicdn.com/imgextra/i1/6000000002914/TB2X884EXOWBuNjy0FiXXXFxVXa_!!6000000002914-0-tbvideo.jpg',
-      caption: 'Búsqueda',
-      href: '/productos;busqueda=tramontina'
+      url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1543814304/slideshow/suprabond.jpg',
+      href: '/productos;busqueda=suprabond'
     },
+    {
+      url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1543814304/slideshow/tacsa.jpg',
+      href: '/productos;busqueda=tacsa'
+    },
+    {
+      url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1543814304/slideshow/interelec.jpg',
+      href: '/productos;busqueda=interelec'
+    }
   ];
 
   constructor(private productosService: ProductosService,
