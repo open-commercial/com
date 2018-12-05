29092f0fef66acaa7d3438776c2673134bad628e
Franco Bellucci <belluccifranco@gmail.com>
2018-12-04 21:24:53 -0300

    Quita los links de las imagenes del slideshow. Reduce el tamanio del slideshow

--- src/app/components/productos/productos.component.ts
+++ src/app/components/productos/productos.component.ts
@@ -22,19 +22,19 @@ export class ProductosComponent implements OnInit, OnDestroy {
   imageUrls: (string | IImage)[] = [
     {
       url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1543814304/slideshow/gladiator-pro.png',
-      href: '/productos;busqueda=gladiator'
+      // href: '/productos;busqueda=gladiator'
     },
     {
       url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1543814304/slideshow/suprabond.jpg',
-      href: '/productos;busqueda=suprabond'
+      // href: '/productos;busqueda=suprabond'
     },
     {
       url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1543814304/slideshow/tacsa.jpg',
-      href: '/productos;busqueda=tacsa'
+      // href: '/productos;busqueda=tacsa'
     },
     {
       url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1543814304/slideshow/interelec.jpg',
-      href: '/productos;busqueda=interelec'
+      // href: '/productos;busqueda=interelec'
     }
   ];
 
