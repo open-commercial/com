0baadef7301e256d36711318e580f80b4ed3f98d
Franco Bellucci <belluccifranco@gmail.com>
2018-12-07 01:01:54 -0300

    Agrega nuevas imagenes al slideshow

--- src/app/components/productos/productos.component.ts
+++ src/app/components/productos/productos.component.ts
@@ -33,9 +33,25 @@ export class ProductosComponent implements OnInit, OnDestroy {
       // href: '/productos;busqueda=tacsa'
     },
     {
-      url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1543814304/slideshow/interelec.jpg',
+      url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1544154497/slideshow/interelec.jpg',
       // href: '/productos;busqueda=interelec'
-    }
+    },
+    {
+      url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1544153960/slideshow/PoxipolLogo1.png',
+      // href: '/productos;busqueda=poxipol'
+    },
+    {
+      url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1544153960/slideshow/tramontina.png',
+      // href: '/productos;busqueda=tramontina'
+    },
+    {
+      url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1544153960/slideshow/brogas.jpg',
+      // href: '/productos;busqueda=brogas'
+    },
+    {
+      url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1544153960/slideshow/bulit.jpg',
+      // href: '/productos;busqueda=bulit'
+    },
   ];
 
   constructor(private productosService: ProductosService,
