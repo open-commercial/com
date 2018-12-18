e8f875c496d2a806ac3746a17e0128c92fc14df2
Facundo Sosa López <fssl2004@gmail.com>
2018-12-18 01:10:59 -0300

    Agrega la grilla de marcas y el footer al Home.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,6 +10,40 @@ import {SlideshowService} from '../../services/slideshow.service';
 export class HomeComponent implements OnInit {
 
   imageUrls: (string | IImage)[] = [];
+  marcas = [
+    {
+      url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1543814304/slideshow/gladiator-pro.png',
+      href: '/productos;busqueda=gladiator'
+    },
+    {
+      url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1543814304/slideshow/suprabond.jpg',
+      href: '/productos;busqueda=suprabond'
+    },
+    {
+      url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1543814304/slideshow/tacsa.jpg',
+      href: '/productos;busqueda=tacsa'
+    },
+    {
+      url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1544154497/slideshow/interelec.jpg',
+      href: '/productos;busqueda=interelec'
+    },
+    {
+      url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1544153960/slideshow/PoxipolLogo1.png',
+      href: '/productos;busqueda=poxipol'
+    },
+    {
+      url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1544153960/slideshow/tramontina.png',
+      href: '/productos;busqueda=tramontina'
+    },
+    {
+      url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1544153960/slideshow/brogas.jpg',
+      href: '/productos;busqueda=brogas'
+    },
+    {
+      url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1544153960/slideshow/bulit.jpg',
+      href: '/productos;busqueda=bulit'
+    },
+  ];
 
   constructor(private slideshowService: SlideshowService) {}
 
--- src/app/services/slideshow.service.ts
+++ src/app/services/slideshow.service.ts
@@ -13,20 +13,12 @@ export class SlideshowService {
     return [
       {
         url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1543814304/slideshow/gladiator-pro.png',
-        // href: '/productos;busqueda=gladiator'
+        /* href='/productos;busqueda=gladiator'*/
       },
       {
         url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1543814304/slideshow/suprabond.jpg',
-        // href: '/productos;busqueda=suprabond'
+        /*href='/productos;busqueda=suprabond'*/
       },
-      {
-        url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1543814304/slideshow/tacsa.jpg',
-        // href: '/productos;busqueda=tacsa'
-      },
-      {
-        url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1543814304/slideshow/interelec.jpg',
-        // href: '/productos;busqueda=interelec'
-      }
     ];
   }
 }
