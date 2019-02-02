32dbe45aef9a083817d51c195188036e7a07b465
Facundo Sosa López <fssl2004@gmail.com>
2019-02-02 00:08:52 -0300

    Cambia las imágenes del logo. Agrega nueva imagen de oferta, y acción de búsqueda al hacer de click.

--- src/app/services/slideshow.service.ts
+++ src/app/services/slideshow.service.ts
@@ -2,24 +2,21 @@ import {Injectable} from '@angular/core';
 import {environment} from 'environments/environment';
 import {HttpClient} from '@angular/common/http';
 import {IImage} from 'ng-simple-slideshow';
+import {Router} from '@angular/router';
 
 @Injectable()
 export class SlideshowService {
 
   url = environment.apiUrl;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,private router: Router) {}
 
   getSlideshowDataForDesktop(): (string | IImage)[] {
     return [
       {
-        url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1546837035/slideshow/ofertaConservadora1280x600.jpg',
-      },
-      {
-        url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1546840178/slideshow/ofertaDesmalezadora1280x600.jpg',
-      },
-      {
-        url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1546842909/slideshow/ofertaPala1280x600.jpg',
+        url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1549072680/slideshow/ofertaPalaTramontina-1280x550px.png',
+        // href: '/productos;busqueda=pala%20tramontina%20emp%20plast'
+        clickAction: () => this.router.navigate(['/productos', { q: 'pala tramontina emp plast'}])
       }
     ];
   }
@@ -27,13 +24,8 @@ export class SlideshowService {
   getSlideshowDataForMobile(): (string | IImage)[] {
     return [
       {
-        url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1546833123/slideshow/ofertaConservadora800x450.jpg',
-      },
-      {
-        url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1546841267/slideshow/ofertaDesmalezadora800x450.jpg',
-      },
-      {
-        url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1546843709/slideshow/ofertaPala800x450.jpg',
+        url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1549072679/slideshow/ofertaPalaTramontina-850x450px.png',
+        clickAction: () => this.router.navigate(['/productos', { q: 'pala tramontina emp plast'}])
       }
     ];
   }
