7fce9cb48212300ae12f6f9190f779b2a23f6cee
Franco Bellucci <belluccifranco@gmail.com>
2019-02-06 21:39:48 -0300

    Cambia las referencias a las imagenes de cloudinary

--- src/app/services/slideshow.service.ts
+++ src/app/services/slideshow.service.ts
@@ -1,6 +1,5 @@
 import {Injectable} from '@angular/core';
 import {environment} from 'environments/environment';
-import {HttpClient} from '@angular/common/http';
 import {IImage} from 'ng-simple-slideshow';
 import {Router} from '@angular/router';
 
@@ -9,13 +8,12 @@ export class SlideshowService {
 
   url = environment.apiUrl;
 
-  constructor(private http: HttpClient,private router: Router) {}
+  constructor(private router: Router) {}
 
   getSlideshowDataForDesktop(): (string | IImage)[] {
     return [
       {
-        url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1549072680/slideshow/ofertaPalaTramontina-1280x550px.png',
-        // href: '/productos;busqueda=pala%20tramontina%20emp%20plast'
+        url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1549499114/slideshow/oferta-pala-1280x550px.png',
         clickAction: () => this.router.navigate(['/productos', { q: 'pala tramontina emp plast'}])
       }
     ];
@@ -24,7 +22,7 @@ export class SlideshowService {
   getSlideshowDataForMobile(): (string | IImage)[] {
     return [
       {
-        url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1549072679/slideshow/ofertaPalaTramontina-850x450px.png',
+        url: 'https://res.cloudinary.com/hf0vu1bg2/image/upload/v1549499113/slideshow/oferta-pala-850x450px.png',
         clickAction: () => this.router.navigate(['/productos', { q: 'pala tramontina emp plast'}])
       }
     ];
