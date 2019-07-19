4b69adc5ac81bd518f791b866b3d2ec1c734a32c
Franco Bellucci <belluccifranco@gmail.com>
2019-07-19 03:16:51 -0300

    Agrega la public key de mercadopago

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://globo-sic-api-production.herokuapp.com',
   idEmpresa: 1,
-  mercadoPagoPublicKey: ''
+  mercadoPagoPublicKey: 'APP_USR-c386f52c-2f15-47c3-be07-700513fef746'
 };
