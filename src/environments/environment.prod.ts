a7e558576c20ffcb46237ff54f09c5160e45742a
Franco Bellucci <belluccifranco@gmail.com>
2020-01-14 00:16:09 -0300

    New app version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   apiUrl: 'https://globo-sic-api-production.herokuapp.com',
   idSucursal: 1,
   mercadoPagoPublicKey: 'APP_USR-c386f52c-2f15-47c3-be07-700513fef746',
-  appVersion: 1,
+  appVersion: 2,
   appName: 'SIC_COM'
 };
