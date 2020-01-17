e52c7eace1ba0ecd71098f6b70264fce6949d0b0
Facundo Sosa López <fssl2004@gmail.com>
2020-01-16 21:00:56 -0300

    Pasa el footer a su propio componente y lo reutiliza en los componentes de login, registracion y password recoveryrequest.

--- src/app/sic-com.module.ts
+++ src/app/sic-com.module.ts
@@ -56,6 +56,7 @@ import {MercadoPagoComponent} from './components/mercado-pago/mercado-pago.compo
 import {PagosService} from './services/pagos.service';
 import {GtagModule} from 'angular-gtag';
 import { CompraRealizadaComponent } from './components/compra-realizada/compra-realizada.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 registerLocaleData(localeEs, 'es-AR', localeEsExtra);
 
@@ -86,6 +87,7 @@ registerLocaleData(localeEs, 'es-AR', localeEsExtra);
     AgregarAlCarritoDialogComponent,
     MercadoPagoComponent,
     CompraRealizadaComponent,
+    FooterComponent,
   ],
   imports: [
     GtagModule.forRoot({ trackingId: 'UA-132433044-1', trackPageviews: true }),
