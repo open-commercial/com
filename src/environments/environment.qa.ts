40abe901ba304117e82f15baf892f4968cb04d82
Franco Bellucci <belluccifranco@gmail.com>
2020-02-07 00:54:36 -0300

    Agrega nueva config para QA environment

---
+++ src/environments/environment.qa.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://globo-sic-api-qa.herokuapp.com',
+  idSucursal: 1,
+  mercadoPagoPublicKey: 'TEST-9f6ef991-ca69-4960-988c-5152623013a1',
+  appVersion: 5,
+  appName: 'SIC_COM'
+};
