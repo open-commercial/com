e0a92eca6e7f852a17b6c52f49e1ed9a6c5c515d
Franco Bellucci <belluccifranco@gmail.com>
2020-04-04 00:15:34 -0300

    Quita el random del appVersion en todos los env

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   apiUrl: 'https://globo-sic-api-qa.herokuapp.com',
   idSucursal: 1,
-  appVersion: Math.floor(Math.random() * 100), // random entre 0 y 100
+  appVersion: 10,
   appName: 'SIC_COM'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,6 +7,6 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080',
   idSucursal: 1,
-  appVersion: Math.floor(Math.random() * 100), // random entre 0 y 100
+  appVersion: 10,
   appName: 'SIC_COM'
 };
