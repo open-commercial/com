10ceb84ed80d38d017c24eb9667fb140e3f1db4c
Franco Bellucci <belluccifranco@gmail.com>
2020-07-04 03:07:47 -0300

    Cambia el app version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   apiUrl: 'https://globo-sic-api-production.herokuapp.com',
   idSucursal: 1,
-  appVersion: 10,
+  appVersion: 12,
   appName: 'SIC_COM'
 };
