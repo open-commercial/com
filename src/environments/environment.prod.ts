3f545e2bf71b600af834a69ea59a0e5fdc65269d
Franco Bellucci <belluccifranco@gmail.com>
2020-08-20 20:37:47 -0300

    Cambia la uri de la api en el env de prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://globo-sic-api-production.herokuapp.com',
+  apiUrl: 'https://api.globodeoro.com',
   idSucursal: 1,
   appVersion: 12,
   appName: 'SIC_COM'
