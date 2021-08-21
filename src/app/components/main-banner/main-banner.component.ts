700887e6d3a5e40bfe02904c5dc5df1912ae958d
Facundo Sosa <fssl2004@gmail.com>
2021-08-21 00:03:03 -0300

    Agrega el componente main-banner.

---
+++ src/app/components/main-banner/main-banner.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sic-com-main-banner',
+  templateUrl: './main-banner.component.html',
+  styleUrls: ['./main-banner.component.scss']
+})
+export class MainBannerComponent {
+}
